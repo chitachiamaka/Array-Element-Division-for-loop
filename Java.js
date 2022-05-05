6518e07d94ecd4ef686e422384aa8244d0351699
chitachiamaka <chitachiamaka.github@gmail.com>
2022-05-05 17:38:03 +0100

    second commit

--- Java.js
+++ Java.js
@@ -1,24 +1,25 @@
 // Question1:
 //Divide the elements in array1 with elements in array2 respectively and store their results in a new array.
 
+
+//Am using using map.( function
+
 let array1 = [20, 46, 90, 80, 120];
 let array2 = [2, 2, 5, 4, 2];
-let array3 = []
-array3 = array1[0]/array2[0];
-var output = array3;
+
+var output = array1.map((array1,i)=>array1/array2[i]);
 console.log(output)
-array3 = array1[1]/array2[1];
-var output2 = array3;
-console.log(output2)
-array3 = array1[2]/array2[2];
-var output3 = array3;
-console.log(output3)
-array3 = array1[3]/array2[3];
-var output4 = array3;
-console.log(output4)
-array3 = array1[4]/array2[4];
-var output5 = array3;
-console.log(output5)
+//i is index of the array delcare
+// the new array ie the result will be store in array1
+
+var arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+for(var i = 0; i < arr.length; i++){
+console.log(arr[5][0][0])
+console.log(arr[3])
+console.log(arr[5][1])
+console.log(arr[1][0])
+
+}
 // ? Question 2:Multi dimensional Arrays
 // *print out these numbers (10,8,13,136 and 4)from the array provided above using for loop
 
