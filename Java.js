6f8d32076055d5690ce41ee7873e8e8c384b5830
chitachiamaka <chitachiamaka.github@gmail.com>
2022-05-05 13:07:16 +0100

    first commit

---
+++ Java.js
@@ -0,0 +1,31 @@
+// Question1:
+//Divide the elements in array1 with elements in array2 respectively and store their results in a new array.
+
+let array1 = [20, 46, 90, 80, 120];
+let array2 = [2, 2, 5, 4, 2];
+let array3 = []
+array3 = array1[0]/array2[0];
+var output = array3;
+console.log(output)
+array3 = array1[1]/array2[1];
+var output2 = array3;
+console.log(output2)
+array3 = array1[2]/array2[2];
+var output3 = array3;
+console.log(output3)
+array3 = array1[3]/array2[3];
+var output4 = array3;
+console.log(output4)
+array3 = array1[4]/array2[4];
+var output5 = array3;
+console.log(output5)
+// ? Question 2:Multi dimensional Arrays
+// *print out these numbers (10,8,13,136 and 4)from the array provided above using for loop
+
+
+
+
+
+
+
+
